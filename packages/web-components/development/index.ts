4705b66c62e2d9be17d5ea682837e472b78cdaf0
SWH-Relewise <swh@relewise.com>
2023-09-04 09:28:09 +0200

    Comment in template overwriting

--- packages/web-components/development/index.ts
+++ packages/web-components/development/index.ts
@@ -1,5 +1,6 @@
 import { UserFactory } from '@relewise/client';
 import { initializeRelewiseUI } from '../src/index';
+import { nothing } from 'lit-html';
 
 initializeRelewiseUI(
     {
@@ -16,21 +17,21 @@ initializeRelewiseUI(
         clientOptions: {
             serverUrl: import.meta.env.VITE_SERVER_URL,
         },
-        // templates: {
-        //     product: (product, { html, helpers }) => {
-        //         return html`
-        //             <div style="border:1px solid black;" class="tile">
-        //                 <div style='height: 5rem'>
-        //                     <h5>${product.displayName}</h5>
-        //                     <span>${product.salesPrice} NOK</span>
-        //                 </div>
-        //                 <div>${helpers.formatPrice(product.salesPrice)}</div>
-        //                 ${(product.data && 'ImageUrl' in product.data)
-        //                     ? html`<div><img src=${product.data['ImageUrl'].value} /></div>`
-        //                     : nothing}
-        //             </div>
-        //     `;
-        //     },
-        // },
+        templates: {
+            product: (product, { html, helpers }) => {
+                return html`
+                    <div style="border:1px solid black;" class="tile">
+                        <div style='height: 5rem'>
+                            <h5>${product.displayName}</h5>
+                            <span>${product.salesPrice} NOK</span>
+                        </div>
+                        <div>${helpers.formatPrice(product.salesPrice)}</div>
+                        ${(product.data && 'ImageUrl' in product.data)
+                            ? html`<div><img src=${product.data['ImageUrl'].value} /></div>`
+                            : nothing}
+                    </div>
+            `;
+            },
+        },
     },
 );
