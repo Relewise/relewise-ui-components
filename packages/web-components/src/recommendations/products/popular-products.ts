7e0481b126c25793fee5db8cef6310f08ac96d7d
SWH-Relewise <swh@relewise.com>
2023-09-08 08:00:04 +0200

    Add empty location when none given

--- packages/web-components/src/recommendations/products/popular-products.ts
+++ packages/web-components/src/recommendations/products/popular-products.ts
@@ -17,7 +17,7 @@ export class PopularProducts extends ProductRecommendationBase {
         const recommender = getRecommender(getRelewiseUIOptions());
         const builder = getProductRecommendationBuilderWithDefaults<PopularProductsBuilder>(
             settings => new PopularProductsBuilder(settings),
-            this.displayedAtLocation,
+            this.displayedAtLocation ?? '',
         )
             .sinceMinutesAgo(this.sinceMinutesAgo)
             .basedOn(this.basedOn)
