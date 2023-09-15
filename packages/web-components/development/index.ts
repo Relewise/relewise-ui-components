a51490d3e73470b4727045c9fab6b1a531c364c3
SWH-Relewise <swh@relewise.com>
2023-09-15 14:05:49 +0200

    Add more css variables

--- packages/web-components/development/index.ts
+++ packages/web-components/development/index.ts
@@ -1,6 +1,5 @@
 import { UserFactory } from '@relewise/client';
 import { initializeRelewiseUI } from '../src/index';
-import { nothing } from 'lit-html';
 
 initializeRelewiseUI(
     {
@@ -15,29 +14,6 @@ initializeRelewiseUI(
         apiKey: import.meta.env.VITE_API_KEY,
         clientOptions: {
             serverUrl: import.meta.env.VITE_SERVER_URL,
-        },
-        templates: {
-            product: (product, { html, helpers }) => {
-                return html`
-                    <div style="border:1px solid black;" class="tile">
-                        <div style='height: 5rem'>
-                            <h5>${product.displayName}</h5>
-                            <span>${product.salesPrice} NOK</span>
-                        </div>
-                        <div>${helpers.formatPrice(product.salesPrice)}</div>
-                        ${(product.data && 'ImageUrl' in product.data)
-                            ? html`<div><img src=${product.data['ImageUrl'].value} /></div>`
-                            : nothing}
-                    </div>
-            `;
-            },
-        },
-        filters: {
-            product: (builder) => {
-                builder
-                    .addProductCategoryIdFilter('ImmediateParent', ['Aktivitetshæfter til Bryllup'])
-                    .addBrandIdFilter(['opgaver for born']);
-            },
         },
     },
 );
