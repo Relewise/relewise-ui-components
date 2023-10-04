c79ae1186bcbb434bced8e5ac9eb336a5bbda4df
SWH-Relewise <swh@relewise.com>
2023-10-04 11:43:37 +0200

    Ensure grid can be styled directly on component

--- packages/web-components/src/recommendations/products/product-recommendation-base.ts
+++ packages/web-components/src/recommendations/products/product-recommendation-base.ts
@@ -34,16 +34,14 @@ export abstract class ProductRecommendationBase extends LitElement {
 
     render() {
         if (this.products) {
-            return html`<div class="rw-grid">
-                ${this.products.map(product =>
+            return html`${this.products.map(product =>
                 html`<relewise-product-tile .product=${product}></relewise-product-tile>`)
-                }
-            </div>`
+                }`;
         }
     }
 
     static styles = css`
-        .rw-grid {
+        :host {
             display: grid;
             grid-template-columns: var(--relewise-grid-template-columns, repeat(4,1fr));
             gap: var(--relewise-grid-gap, 1rem);
@@ -51,7 +49,7 @@ export abstract class ProductRecommendationBase extends LitElement {
         }
 
         @media (max-width: 768px) {
-            .rw-grid {
+            :host {
                 grid-template-columns:var(--relewise-mobile-grid-template-columns, repeat(2,1fr));
             }
         }`;
