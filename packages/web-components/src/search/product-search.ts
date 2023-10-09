8a953fedae45d1b6a138240f037591c74b774a58
SWH-Relewise <swh@relewise.com>
2023-10-09 14:41:59 +0200

    Added search bar to product search

--- packages/web-components/src/search/product-search.ts
+++ packages/web-components/src/search/product-search.ts
@@ -1,14 +1,28 @@
 import { LitElement, css, html } from 'lit';
+import { property, state } from 'lit/decorators.js';
 import { theme } from '../theme';
 
 export class ProductSearch extends LitElement {
+
+    @property({ attribute: 'search-bar-placeholder' })
+    searchBarPlaceholder: string | null = null;
     
+    @state()
+    term: string = '';
+
     async connectedCallback() {
         super.connectedCallback();
     }
 
     render() {
-        return html`Relewise Product Search`;
+        return html`
+        <relewise-search-bar 
+                .term=${this.term}
+                .setSearchTerm=${(term: string)=> this.term = term}
+                .placeholder=${this.searchBarPlaceholder}
+                ></relewise-search-bar>
+        <div>${this.term}</div>
+        `;
     }
 
     static styles = [theme,css``];
