02d836df8b6dc7d33b067c856617f468f7293984
SWH-Relewise <swh@relewise.com>
2023-10-12 12:40:04 +0200

    Handle category facet bug

--- packages/web-components/src/search/components/facets/category-facet.ts
+++ packages/web-components/src/search/components/facets/category-facet.ts
@@ -1,5 +1,5 @@
-import { CategoryFacetResult, ProductSearchResponse } from '@relewise/client';
-import { LitElement, css, html, nothing } from 'lit';
+import { CategoryFacetResult, CategoryNameAndIdResultAvailableFacetValue, ProductSearchResponse } from '@relewise/client';
+import { LitElement, PropertyValues, css, html, nothing } from 'lit';
 import { property, state } from 'lit/decorators.js';
 import { categoryFacetQueryName, updateUrlStateValues } from '../../../helpers';
 import { theme } from '../../../theme';
@@ -9,6 +9,9 @@ export class CategoryFacet extends LitElement {
     @property({ type: Object, attribute: 'search-result' })
     searchResult: ProductSearchResponse | null = null;
 
+    @property({ attribute: 'label-text' })
+    labelText: string = 'Categories';
+
     @state()
     categoryFacet: CategoryFacetResult | null = null;
 
@@ -17,32 +20,46 @@ export class CategoryFacet extends LitElement {
 
     connectedCallback(): void {
         super.connectedCallback();
+    }
 
-        this.categoryFacet = this.searchResult?.facets?.items?.find(x => x.field === 'Category') as CategoryFacetResult;
+    updated(changedProperties: PropertyValues) {
+        super.updated(changedProperties);
+        if (changedProperties.has('searchResult')) {
+            this.categoryFacet = this.searchResult?.facets?.items?.find(x => x.field === 'Category') as CategoryFacetResult;
+        }
+    }
+
+    handleChange(e: Event, item: CategoryNameAndIdResultAvailableFacetValue) {
+        const checkbox = e.target as HTMLInputElement;
+        
+        if (!item.value || !item.value.displayName)  {
+            return;
+        }
+
+        if (checkbox.checked) {
+            this.selectedValues.push(item.value.displayName);
+        } else {
+            const newValue =  this.selectedValues.filter(x => x !== item.value?.displayName);
+            console.log(newValue);
+            this.selectedValues = newValue;
+        }
+
+        updateUrlStateValues(categoryFacetQueryName, this.selectedValues);
     }
 
     render() {
-        console.log('rendered', this.categoryFacet?.available);
         return html`
-        <h1>Category Facet</h1>
-        ${this.categoryFacet ? html`
-            ${this.categoryFacet.available?.map((item, index) => {
+        ${this.categoryFacet && this.categoryFacet.available && this.categoryFacet.available.length > 0 ? html`
+        <h3>${this.labelText}</h3>
+        ${this.categoryFacet.available.map((item, index) => {
                 return html`
-                ${item.value ? html`
+                ${item.value && item.value.displayName ? html`
                     <div>
-                        <input type="checkbox" id=${index} name=${index} @change=${(e: Event) => {
-                            const checkbox = e.target as HTMLInputElement;
-
-                                if (checkbox.checked) {
-                                    this.selectedValues.push(item.value.id);
-                                } else {
-                                    const newValue =  this.selectedValues.filter(x => x !== item.value.id);
-                                    console.log(newValue);
-                                    this.selectedValues = newValue;
-                                }
-    
-                                updateUrlStateValues(categoryFacetQueryName, this.selectedValues);
-                          }} />
+                        <input
+                            type="checkbox"
+                            id=${index}
+                            name=${index}
+                            @change=${(e: Event) => this.handleChange(e, item)} />
                         <label for=${index}>${item.value?.displayName}</label>
                     </div>
                 ` : nothing}
