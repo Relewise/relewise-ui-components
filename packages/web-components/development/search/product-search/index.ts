ebe8c0b3fa9ed3e3e3d3367ea55d7c8298292574
SWH-Relewise <swh@relewise.com>
2023-10-27 10:26:26 +0200

    Simplify setup on search dev page

--- packages/web-components/development/search/product-search/index.ts
+++ packages/web-components/development/search/product-search/index.ts
@@ -23,41 +23,7 @@ initializeRelewiseUI(
         facetBuilder(builder) {
             builder
                 .addCategoryFacet('ImmediateParent')
-                .addBrandFacet()
-                .addProductDataStringValueFacet('Serie', 'Product')
-                .addProductDataStringValueFacet('Color', 'Product')
-                .addProductDataBooleanValueFacet('InStock', 'Product')
-                .addSalesPriceRangeFacet('Product')
-                //.addProductAssortmentFacet('Product')
-                //.addListPriceRangesFacet('Product', [{lowerBound: 1, upperBound: 1000}], 1000,  [{lowerBound: 1, upperBound: 1000}])
-                .addListPriceRangeFacet('Product')
-                //.addProductDataDoubleRangesFacet('RoundingValue', 'Product', [{lowerBound: 1, upperBound: 10}], 10)
-                .addProductDataDoubleRangeFacet('RoundingValue', 'Product')
-                .addProductDataDoubleValueFacet('RoundingValue', 'Product');
-            //.addProductDataObjectFacet()
-            //.addVariantSpecificationFacet()
-        },
-    },
-    localization: {
-        facets: {
-            saveButton: 'Gem',
-            showLessButton: 'Vis færre',
-            showMoreButton: 'Vis flere',
-            toggleButton: 'Filter',
-        },
-        loadMoreButton: {
-            button: 'Hent flere',
-        },
-        searchBar: {
-            placeholder: 'Søg her',
-            searchButton: 'Søg',
-        },
-        sortingButton: {
-            popularityOption: 'populære',
-            alphabeticalAscendingOption: 'a - å',
-            alphabeticalDescendingOption: 'å - a',
-            salesPriceAscendingOption: 'Billigste først',
-            salesPriceDescendingOption: 'Dyreste først',
+                .addBrandFacet();
         },
     },
 });
