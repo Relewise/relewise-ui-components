c1c00247ec4b87126455966d51212c0537f99b15
SWH-Relewise <swh@relewise.com>
2023-12-20 12:50:48 +0100

    Fix tests

--- packages/web-components/web-test-runner-config.mjs
+++ packages/web-components/web-test-runner-config.mjs
@@ -3,22 +3,22 @@ import { esbuildPlugin } from '@web/dev-server-esbuild';
 import { config } from 'dotenv';
 
 config();
-const { INTEGRATION_TEST_DATASET_ID: INTEGRATION_TEST_DATASET_ID, INTEGRATION_TEST_API_KEY: INTEGRATION_TEST_API_KEY} = process.env;
+const { INTEGRATION_TEST_DATASET_ID: INTEGRATION_TEST_DATASET_ID, INTEGRATION_TEST_API_KEY: INTEGRATION_TEST_API_KEY } = process.env;
 const mode = process.env.MODE || 'dev';
 if (!['dev', 'prod'].includes(mode)) {
     throw new Error(`MODE must be "dev" or "prod", was "${mode}"`);
 }
 
 const browsers = {
-    chromium: playwrightLauncher({product: 'chromium'}),
-    firefox: playwrightLauncher({product: 'firefox'}),
-    webkit: playwrightLauncher({product: 'webkit'}),
+    chromium: playwrightLauncher({ product: 'chromium' }),
+    firefox: playwrightLauncher({ product: 'firefox' }),
+    webkit: playwrightLauncher({ product: 'webkit' }),
 };
 
 export default {
     files: ['tests/**/*.test.ts'],
     browsers: Object.values(browsers),
-    nodeResolve: {exportConditions: mode === 'dev' ? ['development'] : []},
+    nodeResolve: { exportConditions: mode === 'dev' ? ['development'] : [] },
     preserveSymlinks: true,
     testFramework: {
         // https://mochajs.org/api/mocha
@@ -35,5 +35,5 @@ export default {
                 <script type="module" src="${testFramework}"></script>
             </body>
          </html>`,
-};  
+};
 
