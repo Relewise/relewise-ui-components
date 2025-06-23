30cdd553492a8d34ced6d63313d65d036340bfef
Simon Walenkamp Hansen <swh@relewise.com>
2025-06-23 07:30:03 +0200

    fix tests

--- packages/web-components/tests/productRecommendationBuilder.test.ts
+++ packages/web-components/tests/productRecommendationBuilder.test.ts
@@ -5,23 +5,23 @@ import { defaultProductProperties } from '../src/defaultProductProperties';
 import { mockRelewiseOptions } from './util/mockRelewiseUIOptions';
 
 suite('productRecommendationBuilder', () => {
-    test('getProductRecommendationBuilderWithDefaults returns builder with defaults if no selectedPropertiesSettings provided', () => {
+    test('getProductRecommendationBuilderWithDefaults returns builder with defaults if no selectedPropertiesSettings provided', async() => {
         const mockedRelewiseOptions = mockRelewiseOptions();
         mockedRelewiseOptions.selectedPropertiesSettings = undefined!;
         initializeRelewiseUI(mockedRelewiseOptions);
     
         const expected = new PopularProductsBuilder(getRelewiseContextSettings('web-components-tests')).setSelectedProductProperties(defaultProductProperties).build();
-        const result = getProductRecommendationBuilderWithDefaults<PopularProductsBuilder>(Options => new PopularProductsBuilder(Options), 'web-components-tests').build();
+        const result = (await getProductRecommendationBuilderWithDefaults<PopularProductsBuilder>(Options => new PopularProductsBuilder(Options), 'web-components-tests')).build();
         
         assert.deepEqual(expected.settings.selectedProductProperties, result.settings.selectedProductProperties);
     });
     
-    test('getProductRecommendationBuilderWithDefaults returns builder with options from initializeRelewiseUI', () => {
+    test('getProductRecommendationBuilderWithDefaults returns builder with options from initializeRelewiseUI', async() => {
         const mockedRelewiseOptions = mockRelewiseOptions();
         initializeRelewiseUI(mockedRelewiseOptions);
     
         const expected = new PopularProductsBuilder(getRelewiseContextSettings('web-components-tests')).setSelectedProductProperties(mockedRelewiseOptions.selectedPropertiesSettings!.product!).build();
-        const result = getProductRecommendationBuilderWithDefaults<PopularProductsBuilder>(Options => new PopularProductsBuilder(Options), 'web-components-tests').build();
+        const result = (await getProductRecommendationBuilderWithDefaults<PopularProductsBuilder>(Options => new PopularProductsBuilder(Options), 'web-components-tests')).build();
         
         assert.deepEqual(expected.settings.selectedProductProperties, result.settings.selectedProductProperties);
     });
