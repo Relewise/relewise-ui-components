d6a63e29938173ca9d46b7ece16078594e1f581e
mkr-relewise <mkr@relewise.com>
2025-10-09 11:38:16 +0200

    refactor: expose content exports solely in /content

--- packages/web-components/src/recommendations/content/index.ts
+++ packages/web-components/src/recommendations/content/index.ts
@@ -1,1 +1,2 @@
 export * from './personal-content';
+export * from './content-recommendation-base';
--- packages/web-components/src/recommendations/index.ts
+++ packages/web-components/src/recommendations/index.ts
@@ -1,5 +1,3 @@
 export * from './products';
 export * from './recommender';
 export * from './product-recommendation-batcher';
-export * from './content/personal-content';
-export * from './content/content-recommendation-base';
