98f248d70d140ad45fd7ec2f30f1921704c778cf
mkr-relewise <mkr@relewise.com>
2025-11-19 13:45:37 +0100

    fix: comply with linter

--- packages/web-components/development/recommendation/user-engagement/index.ts
+++ packages/web-components/development/recommendation/user-engagement/index.ts
@@ -7,7 +7,7 @@ initializeRelewiseUI(
     {
         contextSettings: {
             getUser: () => {
-                return UserFactory.byTemporaryId("mza")
+                return UserFactory.byTemporaryId('temp-id');
             },
             language: import.meta.env.VITE_LANGUAGE,
             currency: import.meta.env.VITE_CURRENCY,
@@ -34,6 +34,6 @@ initializeRelewiseUI(
                     },
                 });
             },
-        }
+        },
     },
 ).useRecommendations().useSearch();
