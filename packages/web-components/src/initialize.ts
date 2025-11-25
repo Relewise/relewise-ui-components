0f3aee9710c1f04b15bd7563ff3230d9f4e324fb
mkr-relewise <mkr@relewise.com>
2025-11-25 10:09:10 +0100

    fix: keep object

--- packages/web-components/src/initialize.ts
+++ packages/web-components/src/initialize.ts
@@ -81,7 +81,7 @@ export interface Targets {
 }
 
 export function initializeRelewiseUI(options: RelewiseUIOptions): App {
-    window.relewiseUIOptions = { ...options };
+    window.relewiseUIOptions = options;
     window.relewiseUISearchTargetedConfigurations = new TargetedSearchConfigurations(options.targets?.searchTargets);
     window.relewiseUIRecommendationTargetedConfigurations = new TargetedRecommendationConfigurations(options.targets?.recommendationTargets);
     return new App();
