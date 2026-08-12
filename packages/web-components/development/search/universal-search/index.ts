37d600ce1bd2c7b719ddfc1e30cc228f72e511a8
Søren Ernst <soe@relewise.com>
2026-08-12 15:16:14 +0200

    Refine category recommendation demo

--- packages/web-components/development/search/universal-search/index.ts
+++ packages/web-components/development/search/universal-search/index.ts
@@ -167,9 +167,9 @@ initializeRelewiseUI(
             recommendations: {
                 initial: [
                     { type: 'PopularProducts', title: 'Popular products', take: 10 },
-                    { type: 'PopularProductCategories', title: 'Popular categories', take: 10 },
+                    { type: 'PopularProductCategories', title: 'Popular product categories', take: 5 },
                     { type: 'PopularContents', title: 'Popular content', take: 10 },
-                    { type: 'PopularContentCategories', title: 'Popular content categories', take: 10 },
+                    { type: 'PopularContentCategories', title: 'Popular content categories', take: 5 },
                     { type: 'PopularSearchTerms', title: 'Popular searches', take: 10 },
                 ],
                 noResults: {
