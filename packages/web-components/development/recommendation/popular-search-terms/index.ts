ec66af05b2238e5e49ae15999f012a4e6941b896
Søren Ernst <soe@relewise.com>
2026-08-17 13:27:22 +0200

    docs: make search term example declarative

--- packages/web-components/development/recommendation/popular-search-terms/index.ts
+++ packages/web-components/development/recommendation/popular-search-terms/index.ts
@@ -21,12 +21,8 @@ initializeRelewiseUI({
     },
 }).useRecommendations();
 
-const popularSearchTerms = document.createElement('relewise-popular-search-terms') as PopularSearchTerms;
-popularSearchTerms.displayedAtLocation = 'Popular Search Terms development';
-popularSearchTerms.numberOfRecommendations = 10;
-popularSearchTerms.targetEntityTypes = ['Product', 'ProductCategory', 'Content'];
-popularSearchTerms.addEventListener(PopularSearchTermsEvents.termSelected, event => {
+const popularSearchTerms = document.querySelector<PopularSearchTerms>('#popular-search-terms-example');
+popularSearchTerms?.addEventListener(PopularSearchTermsEvents.termSelected, event => {
     const term = (event as CustomEvent<PopularSearchTermsTermSelectedEventDetail>).detail.term;
     console.log('Selected popular search term:', term);
 });
-document.querySelector('#popular-search-terms-example')?.append(popularSearchTerms);
